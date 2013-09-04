94380a19d41c327bd27b24f568ddaa0934320f20
Vadim Tkachenko <vadim@percona.com>
2013-09-03 22:40:51 -0400

    Fix insert workload

--- timeseries/insert.lua
+++ timeseries/insert.lua
@@ -12,7 +12,7 @@ function event(thread_id)
    db_bulk_insert_init("INSERT INTO sensordata(ts, sensor_id, data1,data2,data3,data4,data5,cnt) VALUES")
 
    for j = sensors_per_thread*thread_id, sensors_per_thread*(thread_id+1)-1 do
-	 db_bulk_insert_next("("..init_ts.."," .. j .. ",".. sb_rand_uniform(1,10000).."/"..sb_rand_uniform(1,10000).. ",".. sb_rand_uniform(1,10000).."/"..sb_rand_uniform(1,10000)..",".. sb_rand_uniform(1,10000).."/"..sb_rand_uniform(1,10000)..",".. sb_rand_uniform(1,10000).."/"..sb_rand_uniform(1,10000)..",".. sb_rand_uniform(1,10000)/sb_rand_uniform(1,10000)..","..sb_rand_uniform(30,90)..")")
+	 db_bulk_insert_next("("..init_ts.."," .. j .. ",".. sb_rand(1,1000).."/10,".. sb_rand(1,1000).."/10,".. sb_rand(1,1000).."/10,".. sb_rand(1,1000).."/10,".. sb_rand(1,1000).."/10,"..sb_rand(40,60)..")")
    end
 
    init_ts = init_ts + 1
